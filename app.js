b7f1782268e42acdbd8ba3eb1931be1008fe71e9
Grant Collins <grantcollins03@gmail.com>
2023-08-28 15:02:22 -0400

    dropdown menu complete

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-function activateDropDown(elem) {
-    // elem.
+const dropDownLink = document.querySelector('.drop-down-link');
+const dropDownMenu = document.querySelector('.drop-down-menu');
+const companyLink = dropDownLink.querySelector('a');
+
+function activateDropDown() {
+    companyLink.innerHTML = 'Company<i class="fa-solid fa-angle-up"></i>'
+    dropDownMenu.classList.add('visible');
 }
+
+function disableDropDown() {
+    companyLink.innerHTML = 'Company<i class="fa-solid fa-angle-down"></i>'
+    dropDownMenu.classList.remove('visible');
+}
+
+dropDownLink.addEventListener('mouseover', activateDropDown);
+dropDownLink.addEventListener('mouseout', disableDropDown);
