f41de49cfbd22a055dff3c7be5c9f1e760fc8d1c
Grant Collins <grantcollins03@gmail.com>
2023-08-30 13:39:49 -0400

    refactor JS, setInterval

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// Global variables
+
 const dropDown = document.querySelector('.drop-down');
 const menu = dropDown.querySelector('.menu');
 const companyLink = dropDown.querySelector('a');
 const sidebar = document.querySelector('.nav-links-container');
 const mobileMenu = document.querySelector('.mobile-menu');
+const slides = [ ...document.querySelectorAll('.slide')]
+const indicators = [ ...document.querySelectorAll('.indicator')];
+const nextBtn = document.getElementById('right-arrow');
+const previousBtn = document.getElementById('left-arrow');
+
+// Functions
 
 function toggleDropDown() {
     if (menu.classList.contains('expanded')) {
@@ -25,6 +33,31 @@ function toggleSidebar() {
    }
 }
 
+function moveToSlide(targetSlide) {
+    const currentSlide = document.querySelector('.visible')
+    const currentIndicator = indicators[slides.indexOf(currentSlide)];
+    const targetIndicator = indicators[slides.indexOf(targetSlide)];
+
+    currentSlide.classList.remove('visible');
+    currentIndicator.classList.remove('selected');
+    targetSlide.classList.add('visible');
+    targetIndicator.classList.add('selected');
+}
+
+function nextImage() {
+    const currentSlide = document.querySelector('.visible');
+    const nextSlide = currentSlide.nextElementSibling || slides[0];
+    moveToSlide(nextSlide);
+}
+
+function previousImage() {
+    const currentSlide = document.querySelector('.visible');
+    const previousSlide = currentSlide.previousElementSibling || slides[slides.length - 1];
+    moveToSlide(previousSlide);
+}
+
+// Events
+
 dropDown.addEventListener('mouseover', () => {
     if (window.innerWidth > 1024) toggleDropDown();
 });
@@ -37,57 +70,15 @@ dropDown.addEventListener('click', () => {
 
 mobileMenu.addEventListener('click', toggleSidebar);
 
-
-const slides = document.querySelectorAll('.slide')
-const indicators = document.querySelectorAll('.indicator');
-
-function nextImage() {
-    const currentSlide = [ ...slides].find(slide => slide.classList.contains('visible'));
-    const currentIndex = [ ...slides].indexOf(currentSlide);
-    
-    if (currentIndex === slides.length - 1) {
-        slides[0].classList.add('visible');
-        indicators[0].classList.add('selected');
-    } else {
-        slides[currentIndex + 1].classList.add('visible');
-        indicators[currentIndex + 1].classList.add('selected');
-    }
-    indicators[currentIndex].classList.remove('selected');
-    currentSlide.classList.remove('visible');
-}
-
-function previousImage() {
-    const currentSlide = [ ...slides].find(slide => slide.classList.contains('visible'));
-    const currentIndex = [ ...slides].indexOf(currentSlide);
-
-    if (currentIndex === 0) {
-        slides[slides.length - 1].classList.add('visible');
-        indicators[slides.length - 1].classList.add('selected');
-    } else {
-        slides[currentIndex - 1].classList.add('visible');
-        indicators[currentIndex - 1].classList.add('selected');
-    }
-    indicators[currentIndex].classList.remove('selected');
-    currentSlide.classList.remove('visible');
-}
-
-const nextBtn = document.getElementById('right-arrow');
 nextBtn.addEventListener('click', nextImage);
 
-const previousBtn = document.getElementById('left-arrow');
 previousBtn.addEventListener('click', previousImage);
 
-indicators.forEach(indicator => {
+indicators.forEach((indicator, index) => {
     indicator.addEventListener('click', () => {
-        const currentSlide = [ ...slides].find(slide => slide.classList.contains('visible'));
-        const currentIndex = [ ...slides].indexOf(currentSlide);
-        const clickedIndex = [ ...indicators].indexOf(indicator);
-
-        currentSlide.classList.remove('visible');
-        indicators[currentIndex].classList.remove('selected');
-
-        slides[clickedIndex].classList.add('visible');
-        indicators[clickedIndex].classList.add('selected'); 
-    })
+        moveToSlide(slides[index]);
+    });
 });
+
+setInterval(nextImage, 5000);
 
